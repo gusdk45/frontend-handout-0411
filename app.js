a0a1799f020220bb4c2e0832878e5a0602e49b47
gusdk45 <gusdk1418@naver.com>
2024-02-14 20:41:42 +0900

    5주차 이미지 슬라이더 과제

---
+++ app.js
@@ -0,0 +1,31 @@
+// 이미지 슬라이드 요소와 이전/다음 버튼 요소를 선택합니다.
+
+// 각 이미지 슬라이드의 초기 위치를 설정합니다.
+slides.forEach(function (slide, index) {});
+
+// 현재 이미지 슬라이드의 인덱스를 나타내는 변수를 초기화합니다.
+let counter = 0;
+
+// 다음 버튼 클릭 시 호출되는 이벤트 핸들러를 추가합니다.
+nextBtn.addEventListener("click", function () {
+  // carousel 함수를 호출하여 이미지 슬라이드 및 버튼을 업데이트합니다.
+});
+
+// 이전 버튼 클릭 시 호출되는 이벤트 핸들러를 추가합니다.
+prevBtn.addEventListener("click", function () {
+  // carousel 함수를 호출하여 이미지 슬라이드 및 버튼을 업데이트합니다.
+});
+
+// 이미지 슬라이드 및 버튼 상태를 업데이트하는 carousel 함수를 정의합니다.
+function carousel() {
+  // 다음 버튼 및 이전 버튼의 표시 여부를 결정하여 업데이트합니다.
+  if (counter < slides.length - 1) {
+  } else {
+  }
+  if (counter > 0) {
+  } else {
+  }
+  // 이미지 슬라이드 이동
+}
+
+// 초기 상태에서는 이전 버튼을 숨깁니다.
