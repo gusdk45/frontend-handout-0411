48b75578872afa361642bbc8458c707f433a2a8e
gusdk45 <122820408+gusdk45@users.noreply.github.com>
2024-04-11 16:57:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,24 @@ slides.forEach(function (slide, index) {});
 // 현재 이미지 슬라이드의 인덱스를 나타내는 변수를 초기화합니다.
 let counter = 0;
 
-// 다음 버튼 클릭 시 호출되는 이벤트 핸들러를 추가합니다.
-nextBtn.addEventListener("click", function () {
-  // carousel 함수를 호출하여 이미지 슬라이드 및 버튼을 업데이트합니다.
-});
+// 다음 버튼 클릭 시 호출되는 이벤트 핸들러를 추가합니다.(carousel 함수 사용)
 
-// 이전 버튼 클릭 시 호출되는 이벤트 핸들러를 추가합니다.
-prevBtn.addEventListener("click", function () {
-  // carousel 함수를 호출하여 이미지 슬라이드 및 버튼을 업데이트합니다.
-});
+// 이전 버튼 클릭 시 호출되는 이벤트 핸들러를 추가합니다.(carousel 함수 사용)
 
 // 이미지 슬라이드 및 버튼 상태를 업데이트하는 carousel 함수를 정의합니다.
 function carousel() {
   // 다음 버튼 및 이전 버튼의 표시 여부를 결정하여 업데이트합니다.
   if (counter < slides.length - 1) {
+    
   } else {
+    
   }
   if (counter > 0) {
+    
   } else {
+    
   }
+  
   // 이미지 슬라이드 이동
 }
 
